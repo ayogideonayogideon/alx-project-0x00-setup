2cc3055f63ab92fff1d00e14185d7ada0265cb79
Ekujumi Ayodele Gideon <aeongideonite@gmail.com>
2025-07-31 02:06:41 +0100

    Changes to be committed:     new file:   alx-project-0x01     new file:   components/common/PostCard.tsx     new file:   components/common/button.tsx     new file:   components/layout/Footer.tsx 	new file:   components/layout/Header.tsx 	modified:   pages/index.tsx     new file:   pages/posts/index.tsx     new file:   pages/users/index.tsx     modified:   styles/globals.css     modified:   alx-project-0x01 (modified content)

---
+++ components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <header className="p-4 bg-gray-100 text-center text-xl font-bold">
+      App Header
+    </header>
+  );
+};
+
+export default Header;
