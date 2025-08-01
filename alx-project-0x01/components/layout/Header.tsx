202a06bd1cea115bfe32345b8900753f30e31b10
Ekujumi Ayodele Gideon <aeongideonite@gmail.com>
2025-08-01 05:30:32 +0100

    Add new post interface and components

--- alx-project-0x01/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header: React.FC = () => {
-  return (
-    <header className="p-4 bg-gray-100 text-center text-xl font-bold">
-      App Header
-    </header>
-  );
-};
-
-export default Header;
